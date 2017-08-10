cf5832d4ab070698a8edaa799f8d23afa9f6bec5
Terasaki Shun <t.tera1217@gmail.com>
2017-08-10 10:33:56 +0900

    update compute_w

--- reconstructor.cc
+++ reconstructor.cc
@@ -1,15 +1,23 @@
 #include <opencv2/imgproc.hpp>
-#include <eigen3/Eigen/Dense>
+#include <unsupported/Eigen/FFT>
 #include "./reconstructor.h"
 
 void ImageReconstructor::operator()(const cv::Mat &src_img,cv::Mat &dst_img){
-
+  img_rows_ = src_img.rows;
+  img_cols_ = src_img.cols;
 
   while(check_stop_criterion()){
     compute_w();
     compute_u();
   }
+}
 
+double ImageReconstructor::compute_horizontal_diff(const Eigen::MatrixXd &mat,int r,int c){
+  return mat(r,(c+1)%mat.cols())-mat(r,c);
+}
+
+double ImageReconstructor::compute_vertical_diff(const Eigen::MatrixXd &mat,int r,int c){
+  return mat((r+1)%mat.rows(),c)-mat(r,c);
 }
 
 bool ImageReconstructor::check_stop_criterion(){
@@ -17,7 +25,59 @@ bool ImageReconstructor::check_stop_criterion(){
 }
 
 void ImageReconstructor::compute_w(){
+  for(int r=0;r<img_rows_;++r){
+    for(int c=0;c<img_cols_;++c){
+      double Du_x = compute_horizontal_diff(u_,r,c);
+      double Du_y = compute_vertical_diff(u_,r,c);
+
+      Eigen::Vector2d Du,w_vec;
+      Du << Du_x , Du_y;
+
+      double Du_norm = Du.norm();
+      double coef = Du_norm-1.0/beta_;
+      if(coef > 0){
+        w_horizontal_(r,c) = coef*Du_x/Du_norm;
+        w_vertical_(r,c) = coef*Du_y/Du_norm;
+      }else{
+        w_horizontal_(r,c) = 0;
+        w_vertical_(r,c) = 0;
+      }
+    }
+  }
+
 }
 
 void ImageReconstructor::compute_u(){
 }
+
+void ImageReconstructor::fft2dim(Eigen::MatrixXcd &dst_mat,const Eigen::MatrixXcd &src_mat,bool forward){
+  Eigen::FFT<double> fft;
+  int rows = src_mat.rows();
+  int cols = src_mat.cols();
+
+  Eigen::MatrixXcd tmp_mat(rows,cols);
+  for(int c=0;c<cols;++c){
+    Eigen::VectorXcd src_vec = src_mat.col(c);
+    Eigen::VectorXcd tmp_vec(cols);
+    if(forward){
+      fft.fwd(tmp_vec,src_vec);
+    }else{
+      fft.inv(tmp_vec,src_vec);
+    }
+    tmp_mat.col(c) = tmp_vec;
+  }
+
+  tmp_mat.transposeInPlace();
+  for(int c=0;c<cols;++c){
+    Eigen::VectorXcd tmp_vec = tmp_mat.col(c);
+    Eigen::VectorXcd dst_vec(cols);
+    if(forward){
+      fft.fwd(dst_vec,tmp_vec);
+    }else{
+      fft.inv(dst_vec,tmp_vec);
+    }
+    dst_mat.col(c) = dst_vec;
+  }
+}
+
+
--- reconstructor.h
+++ reconstructor.h
@@ -4,15 +4,20 @@
 #include <Eigen/Dense>
 
 class ImageReconstructor{
+  int img_rows_,img_cols_;
   double beta_,mu_;
   double epsilon_;
 
-  Eigen::MatrixXd w,u;
+  Eigen::MatrixXd w_horizontal_,w_vertical_;
+  Eigen::MatrixXd u_;
 
+  double compute_horizontal_diff(const Eigen::MatrixXd &,int r,int c);
+  double compute_vertical_diff(const Eigen::MatrixXd &,int r,int c);
   bool check_stop_criterion();
   void compute_w();
   void compute_u();
-
+  
+  void fft2dim(Eigen::MatrixXcd &dst_mat,const Eigen::MatrixXcd &src_mat,bool forward=true);
 
   public:
   ImageReconstructor(){}
